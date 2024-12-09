ae4bb14492acc7180527443b69b64ae75370e93e
sjhyoon <yhsean719@gmail.com>
2024-12-08 18:16:29 -0800

    Fix the tag value to be updated and stored in the firebase

--- app/src/main/java/edu/uw/ischool/jtay25/digitalrecipebook/RecipeAdapter.kt
+++ app/src/main/java/edu/uw/ischool/jtay25/digitalrecipebook/RecipeAdapter.kt
@@ -1,4 +1,5 @@
 package edu.uw.ischool.jtay25.digitalrecipebook
+import android.util.Log
 import edu.uw.ischool.jtay25.digitalrecipebook.Recipe
 import android.view.LayoutInflater
 import android.view.View
@@ -18,19 +19,33 @@ class RecipeAdapter(
         private val durationText: TextView = view.findViewById(R.id.duration)
         private val bookmark: ImageView = view.findViewById(R.id.bookmarkIcon)
 
+
         fun bind(recipe: Recipe) {
+            val tagRef = com.google.firebase.database.FirebaseDatabase.getInstance().getReference("recipes").child(recipe.id).child("tag")
+            var currentTag : Boolean
             titleText.text = recipe.title
             chefText.text = "By ${recipe.chef}"
             durationText.text = recipe.duration
-
-            bookmark.setOnClickListener {
-                if(!recipe.tag) {
-                    recipe.tag = true
-                    bookmark.setImageResource(R.drawable.icon_bookmark_added)
-                } else {
-                    recipe.tag = false
-                    bookmark.setImageResource(R.drawable.icon_bookmark)
+            tagRef.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    currentTag = task.result?.value as? Boolean == true
+                    when (currentTag) {
+                        true -> bookmark.setImageResource(R.drawable.icon_bookmark_added)
+                        false -> bookmark.setImageResource(R.drawable.icon_bookmark)
+                    }
+                    bookmark.setOnClickListener {
+                        if (!currentTag) {
+                            // If tag is not true, set it to true (added to bookmarks)
+                            tagRef.setValue(true)
+                            bookmark.setImageResource(R.drawable.icon_bookmark_added)
+                        } else {
+                            // If tag is already true, set it to false (removed from bookmarks)
+                            tagRef.setValue(false)
+                            bookmark.setImageResource(R.drawable.icon_bookmark)
+                        }
+                    }
                 }
+
             }
 
             // Set click listener for navigating to details
